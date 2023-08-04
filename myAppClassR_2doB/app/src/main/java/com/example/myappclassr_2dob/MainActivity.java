eb1de2d4203d40e3a10ba255dfa8a63db369a144
rodriquelali <rodriquelali@gmail.com>
2023-08-04 16:42:41 -0400

    primer proyecto, suma de dos numeros

---
+++ myAppClassR_2doB/app/src/main/java/com/example/myappclassr_2dob/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.myappclassr_2dob;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txt1,txt2;
+    TextView tvR;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt1 = (EditText) findViewById(R.id.num_1);
+    }
+}
